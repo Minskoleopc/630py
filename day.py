ba559e012abbf711eedacbe46f2d184db6f5feee
unknown <Minskoleopc>
2024-02-06 18:55:11 +0530

    added

---
+++ day.py
@@ -0,0 +1,100 @@
+x = 10
+print(x)
+x = 100
+print(x)
+
+# program 2
+# Arithmetic operation -
+# Addition - + 
+# Subtraction - - 
+# Multiplication - *
+# Division   - /
+# Remainder - %
+
+s = 9 
+t = 3
+
+print(s+t)
+print(s-t)
+print(s*t)
+print(s/t)
+print(s%t)
+
+
+q = 8 
+r = 4
+
+print(q+r)
+print(q-r)
+print(q*r)
+print(q/r)
+print(q%r)
+
+
+def Calculator(x,y):
+    print(x+y)
+    print(x-y)
+    print(x*y)
+    print(x/y)
+    print(x%y)
+
+Calculator(12,3)
+Calculator(12,4)
+
+# program 3
+
+# function without parameter and without return  type
+
+def addA():
+    print(9+9)
+addA()
+addA()
+addA()
+addA()
+
+# function with parameter and without return type 
+
+def addB(x,y):
+    print(x+y)
+addB(12,3)
+addB(1,3)
+addB(11,3)
+
+
+# function with parameter and with return 
+# 100 - shown
+# 100 - given
+
+def addC(x,y):
+    return x + y
+e = addC(24,5)
+print(e)
+print(e + e)
+print(e * e)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
