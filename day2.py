ac5c18f9e9756ff266baa13a369e6801dc7dbbf0
unknown <Minskoleopc>
2024-02-07 18:56:32 +0530

    added

---
+++ day2.py
@@ -0,0 +1,95 @@
+print("hello")
+
+
+# introvert    # extrovert
+# calm           loud
+# less outing    more outing
+# less social    more social
+# Human - 
+# Properties - weight , height , color 
+# Methods   - walk() , talk()
+
+# Vehicle 
+# Properties -  color , regNo , type , model 
+# method     - start() , stop()
+
+# Bank acc
+# Propeties - accNo , accType , bal , IFSC
+# Method - deposit() , withdrawl()
+
+
+a = 10
+print(a)
+print(type(a))
+# 10,- 10 , 0
+
+b = 20.5
+print(b)
+print(type(b))
+# 12.5 , -12.5 , 0.45
+
+c = True
+print(c)
+print(type(c))
+# True or False
+
+d = "chinmay"
+print(d)
+print(type(d))
+#"A", "a", '#', "$$","FWEr34543ra@#$", " "
+
+# comparison operator 
+# entity < entity ====> boolean ====> True or False
+# < , > , <= , >= ,== ,!= 
+
+print(6 > 3)
+print(6 < 3)
+print(6 >= 3)
+print(6 <= 3)
+print(6 >= 6)
+print(6 <= 6)
+print(6 != 6)
+print(7 == 7)
+
+# logical operator 
+
+# and  
+# True  and True   =====> True
+# False and True   =====> False
+# True  and False  =====> False
+# False and False  =====> False
+
+print(5 == 5 and 6 == 6)
+print(7 == 8 and 9 == 9)
+print(6 == 6 and 8 == 9)
+print(8 == 9 and 9 == 8)
+
+
+#or  
+
+#not 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
